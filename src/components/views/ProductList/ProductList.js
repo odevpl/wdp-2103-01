f0fd18c80d4100aeaae4616ff7cf72b65a2c9f90
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-03-24 00:23:43 +0100

    Add CategoryFilter

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,10 +2,12 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import Banner from '../../features/Banner/BannerContainer.js';
+import CategoryFilter from '../../features/CategoryFilter/CategoryFilterContainer';
 
 const ProductList = () => (
   <div>
     <Banner />
+    <CategoryFilter />
     <div className={styles.root}>This is ProductList</div>;
   </div>
 );
