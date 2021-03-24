11ce2e1b585d4342f8164daf484f0b4a2c352dc9
anna130993 <73667449+anna130993@users.noreply.github.com>
2021-03-24 21:04:03 +0100

    Delete overflow files

--- src/components/features/CategoryFilter/CategoryFilterContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import CategoryFilter from './CategoryFilter.js';
-import { getAll } from '../../../redux/categoriesRedux';
-
-const mapStateToProps = state => ({
-  categories: getAll(state),
-});
-
-export default connect(mapStateToProps)(CategoryFilter);
