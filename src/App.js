5e21879590ced27e8b844a884ce32185df11c2ff
anna130993 <73667449+anna130993@users.noreply.github.com>
2021-03-25 10:13:52 +0100

    Kick out unnecessary line

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import MainLayout from './components/layout/MainLayout/MainLayoutContainer.js';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
-import Products from './components/views/Products/Products';
 
 const App = () => (
   <Provider store={store}>
